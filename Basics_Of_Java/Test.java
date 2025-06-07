8043bee28ea90349c80f629f814692b33ffcedd3
khadkaabishek <abishekkhadka36@gmail.com>
2025-06-07 20:13:24 +0545

    Basics Of Java

---
+++ Basics_Of_Java/Test.java
@@ -0,0 +1,32 @@
+//Here Use can able to add own exceptions cases 
+//Take an Example of Semester if user input enters the value out of range (lets say 1-8) then program should throw an Error 
+class User_Exception extends Exception {
+    public User_Exception(String msg) {
+        super(msg);
+    }
+}
+
+class Student {
+    String name;
+    int sem;
+
+    public Student(String name, int sem) throws User_Exception {
+        if (sem < 1 || sem > 8) {
+            throw new User_Exception("Invalid Semester");
+        } else {
+            this.name = name;
+            this.sem = sem;
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        try {
+            Student s = new Student("Abishek", 04);
+
+        } catch (User_Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
